6503c0cbf9ff4f754637515e4c6161edf06ca025
joseuisgs <joseluisgonsan@gmail.com>
2021-10-25 17:40:44 +0200

    CommentRepository

--- src/main/java/es/joseluisgs/dam/blog/repository/CommentRepository.java
+++ src/main/java/es/joseluisgs/dam/blog/repository/CommentRepository.java
@@ -1,31 +1,41 @@
 package es.joseluisgs.dam.blog.repository;
 
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.mongodb.client.model.ReturnDocument;
+import es.joseluisgs.dam.blog.database.MongoDBController;
+import es.joseluisgs.dam.blog.model.Category;
 import es.joseluisgs.dam.blog.model.Comment;
 import es.joseluisgs.dam.blog.manager.HibernateController;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
-import javax.persistence.TypedQuery;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class CommentRepository implements CrudRespository<Comment, Long> {
+import static com.mongodb.client.model.Filters.eq;
+
+public class CommentRepository implements CrudRespository<Comment, ObjectId> {
     @Override
     public List<Comment> findAll() {
-        HibernateController hc = HibernateController.getInstance();
-        hc.open();
-        TypedQuery<Comment> query = hc.getManager().createNamedQuery("Comment.findAll", Comment.class);
-        List<Comment> list = query.getResultList();
-        hc.close();
+        MongoDBController mongoController = MongoDBController.getInstance();
+        mongoController.open();
+        MongoCollection<Comment> commentCollection = mongoController.getCollection("blog", "comment", Comment.class);
+        List<Comment> list = commentCollection.find().into(new ArrayList<Comment>());
+        mongoController.close();
         return list;
     }
 
     @Override
-    public Comment getById(Long ID) throws SQLException {
-        HibernateController hc = HibernateController.getInstance();
-        hc.open();
-        Comment comment = hc.getManager().find(Comment.class, ID);
-        hc.close();
+    public Comment getById(ObjectId ID) throws SQLException {
+        MongoDBController mongoController = MongoDBController.getInstance();
+        mongoController.open();
+        MongoCollection<Comment> commentCollection = mongoController.getCollection("blog", "comment", Comment.class);
+        Comment comment = commentCollection.find(eq("_id", ID)).first();
+        mongoController.close();
         if (comment != null)
             return comment;
         throw new SQLException("Error CommentRepository no existe comentario con ID: " + ID);
@@ -35,20 +45,16 @@ public class CommentRepository implements CrudRespository<Comment, Long> {
     public Comment save(Comment comment) throws SQLException {
         UUID uuid = UUID.randomUUID();
         comment.setUuid(uuid.toString());
-        HibernateController hc = HibernateController.getInstance();
-        hc.open();
+        MongoDBController mongoController = MongoDBController.getInstance();
+        mongoController.open();
+        MongoCollection<Comment> commentCollection = mongoController.getCollection("blog", "comment", Comment.class);
         try {
-            hc.getTransaction().begin();
-            hc.getManager().persist(comment);
-            hc.getTransaction().commit();
+            commentCollection.insertOne(comment);
             return comment;
         } catch (Exception e) {
             throw new SQLException("Error CommentRepository al insertar comentario en BD: " + e.getMessage());
         } finally {
-            if (hc.getTransaction().isActive()) {
-                hc.getTransaction().rollback();
-            }
-            hc.close();
+            mongoController.close();
         }
     }
 
@@ -56,42 +62,33 @@ public class CommentRepository implements CrudRespository<Comment, Long> {
     public Comment update(Comment comment) throws SQLException {
         UUID uuid = UUID.randomUUID();
         comment.setUuid(uuid.toString());
-        HibernateController hc = HibernateController.getInstance();
-        hc.open();
+        MongoDBController mongoController = MongoDBController.getInstance();
+        mongoController.open();
+        MongoCollection<Comment> commentCollection = mongoController.getCollection("blog", "comment", Comment.class);
         try {
-            hc.getTransaction().begin();
-            hc.getManager().merge(comment);
-            hc.getTransaction().commit();
-            return comment;
+            Document filtered = new Document("_id", comment.getId());
+            FindOneAndReplaceOptions returnDoc = new FindOneAndReplaceOptions().returnDocument(ReturnDocument.AFTER);
+            return commentCollection.findOneAndReplace(filtered, comment, returnDoc);
         } catch (Exception e) {
             throw new SQLException("Error CommentRepository al actualizar comentario con id: " + comment.getId() + " " + e.getMessage());
         } finally {
-            if (hc.getTransaction().isActive()) {
-                hc.getTransaction().rollback();
-            }
-            hc.close();
+            mongoController.close();
         }
 
     }
 
     @Override
     public Comment delete(Comment comment) throws SQLException {
-        HibernateController hc = HibernateController.getInstance();
-        hc.open();
+        MongoDBController mongoController = MongoDBController.getInstance();
+        mongoController.open();
+        MongoCollection<Comment> commentCollection = mongoController.getCollection("blog", "comment", Comment.class);
         try {
-            hc.getTransaction().begin();
-            // Ojo que borrar implica que estemos en la misma sesión y nos puede dar problemas, por eso lo recuperamos otra vez
-            comment = hc.getManager().find(Comment.class, comment.getId());
-            hc.getManager().remove(comment);
-            hc.getTransaction().commit();
-            return comment;
+            Document filtered = new Document("_id", comment.getId());
+            return commentCollection.findOneAndDelete(filtered);
         } catch (Exception e) {
             throw new SQLException("Error CommentRepository al eliminar comentario con id: " + comment.getId() + " " + e.getMessage());
         } finally {
-            if (hc.getTransaction().isActive()) {
-                hc.getTransaction().rollback();
-            }
-            hc.close();
+            mongoController.close();
         }
     }
 }
